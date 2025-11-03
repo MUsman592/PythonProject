9c010e138c07768f802f24893e6f7605562dace4
M.Usman592 <M.Usman59212@gmail.com>
2025-11-03 18:22:14 +0500

    deleted all question files

--- Python Course/4. question/index.py
+++
@@ -1,9 +0,0 @@
-# Accapt numbers from user 
-
-num = int(input("enter ur number"))
-print("u entered :",num)
-
-# Accapt age from user
-
-age = int(input("Please enter ur age : "))
-print("ur age :",age )
